7c951e5b76626750918e188878baefbb8f5a6acd
Housni <alami.housni@gmail.com>
2022-01-14 22:39:57 +0100

    fetching data done

--- github-repo-finder/src/App.js
+++ github-repo-finder/src/App.js
@@ -1,9 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    if (!inputValue) {
+      return;
+    }
+
+    fetch(`https://api.github.com/search/repositories?q=${inputValue}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setRepos(data.items)
+      });
+
+  }, [inputValue])
   return (
-    <div className="App">
-     
+    <div>
+      <form onSubmit={evt => {
+        evt.preventDefault()
+        setInputValue(evt.target.elements.query.value)
+      }}>
+        <input
+          type="text"
+          name="query"
+          placeholder="Search"
+        />
+      </form>
+      <ul>
+        {repos.map(repo => {
+          return <li key={repo.id}>
+            <a href={repo.url}>{repo.name}</a>
+            <p>{repo.description}</p>
+          </li>
+        })}
+      </ul>
     </div>
   );
 }
